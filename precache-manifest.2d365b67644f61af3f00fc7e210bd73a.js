dee12a48a1b7c1731f865013670f97b02157e7ac
Hamed Rohani <hamedrohani@R-Mac.attlocal.net>
2020-06-19 09:52:48 -0700

    Updates

---
+++ precache-manifest.2d365b67644f61af3f00fc7e210bd73a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3b89a224c803bc675bb614ae71629e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c97e644e6be20511249b",
+    "url": "/static/css/main.e337fac9.chunk.css"
+  },
+  {
+    "revision": "32cc6d1e37c66a94eaf9",
+    "url": "/static/js/2.2aa2977b.chunk.js"
+  },
+  {
+    "revision": "86b3fd007a81eedc141342e4bd6d6c2c",
+    "url": "/static/js/2.2aa2977b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97e644e6be20511249b",
+    "url": "/static/js/main.891449f0.chunk.js"
+  },
+  {
+    "revision": "d1e893df81362b2768d8",
+    "url": "/static/js/runtime-main.f3b168eb.js"
+  },
+  {
+    "revision": "0f039a9afef99cb6c6f45d1106823adc",
+    "url": "/static/media/exampleimg.0f039a9a.jpg"
+  },
+  {
+    "revision": "e1d400309e4298da93d8e32e1e4312b6",
+    "url": "/static/media/hmd.e1d40030.svg"
+  },
+  {
+    "revision": "b65c0a0c9d8a5b559599f547e36f09ff",
+    "url": "/static/media/line.b65c0a0c.svg"
+  }
+]);
